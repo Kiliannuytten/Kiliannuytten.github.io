c9885558222cd2b59f51188304c6003d40974107
Kiliannuytten <kilian.nuytten@student.vives.be>
2023-03-10 12:59:43 +0100

    f

--- labos/JavaScript/labo3/opdracht_producten/scripts/code.js
+++ labos/JavaScript/labo3/opdracht_producten/scripts/code.js
@@ -1,11 +1,38 @@
 const setup = () => {
-    let products = document.getElementsByClassName("aantal");
-    for(let i = 0; i<products.length;i++){
-        products[i].addEventListener("change", calc);
-    }
+    let btnHerbereken = document.getElementById("btnHerbereken");
+    btnHerbereken.addEventListener("click", calc)
 }
 
 const  calc = () => {
 
+    //get prijs van product
+    let txtPrijzen = document.getElementsByClassName("bedrag");
+
+    //get input van aantal producten
+    let txtAantal = document.getElementsByClassName("aantal");
+
+    // get btw van product
+    let txtBtw = document.getElementsByClassName("btw");
+
+    //get subtotaal
+    let txtSubtotaal = document.getElementsByClassName("subtotaal");
+
+    let totaal = 0.0;
+
+    for(let i = 0; i<txtAantal.length;i++){
+        let aantal = parseFloat(txtAantal[i].value);
+        console.log("aantal: " + aantal);
+        let prijs = parseFloat(txtPrijzen[i].innerHTML);
+        console.log("prijs: " + prijs);
+        let btw = parseFloat(txtBtw[i].innerHTML);
+        console.log("btw: " + btw)
+
+        let subtotaal = (prijs + (prijs * (btw/100))) * aantal;
+        console.log("subtotaal: " + subtotaal);
+
+        txtSubtotaal[i].innerHTML = subtotaal + " EUR";
+        totaal += subtotaal;
+    }
+    document.getElementById("totaal").innerHTML = totaal + " EUR"
 }
 window.addEventListener("load", setup);
