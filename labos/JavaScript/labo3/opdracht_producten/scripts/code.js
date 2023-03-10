6855b0a4829c6076e697f664812c37707c6d2b52
Kiliannuytten <kilian.nuytten@student.vives.be>
2023-03-10 13:20:58 +0100

    d

--- labos/JavaScript/labo3/opdracht_producten/scripts/code.js
+++ labos/JavaScript/labo3/opdracht_producten/scripts/code.js
@@ -20,23 +20,33 @@ const  calc = () => {
     let totaal = 0.0;
 
     for(let i = 0; i<txtAantal.length;i++){
+        //aantal van een product op halen
         let aantal = parseFloat(txtAantal[i].value);
         console.log("aantal: " + aantal);
+        //prijs van een product op halen
         let prijs = parseFloat(txtPrijzen[i].innerHTML);
         console.log("prijs: " + prijs);
+        //btw van een product op halen
         let btw = parseFloat(txtBtw[i].innerHTML);
         console.log("btw: " + btw)
 
+        //subtotaal berekenen
         let subtotaal = (prijs + (prijs * (btw/100))) * aantal;
         console.log("subtotaal: " + subtotaal);
+
+        //subtotaal moooi afronden
         subtotaal = subtotaal.toFixed(2);
         subtotaal = parseFloat(subtotaal);
 
+        //subtotaal weergeven in tabel
         txtSubtotaal[i].innerHTML = subtotaal + " EUR";
         totaal += subtotaal;
     }
+
+    //totaal mooi afronden
     totaal = totaal.toFixed(2);
     totaal = parseFloat(totaal);
+    //totaal weergeven in tabel
     document.getElementById("totaal").innerHTML = totaal + " EUR"
 }
 window.addEventListener("load", setup);
